f930737db1c285e7cfa3b8f54799bbd3a54e9d0b
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-10-28 17:00:37 -0300

    method GET

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from app.models import Todo
+from app.serializers import TodoSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def todo_list(request):
+    todo = Todo.objects.all()
+    serializer = TodoSerializer(todo, many=True)
+
+    return Response(serializer.data)
